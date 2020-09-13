60309c790e6118c5c5c60e35776a2c2d2c7ae8f7
Elijah Cobb <elijah@elijahcobb.com>
2020-09-13 12:24:49 -0400

    editor

--- render/src/App.tsx
+++ render/src/App.tsx
@@ -8,15 +8,17 @@
 import * as React from "react";
 import "./index.css";
 import {TopBar} from "./topBar/TopBar";
-import {WelcomeView} from "./welcome/WelcomeView";
 import "./App.css";
+import {Remote} from "electron";
+const remote: Remote = window.require("electron").remote;
+const fs = window.require("fs");
 
 export interface AppProps {
 
 }
 
 export interface AppState {
-
+	value: string;
 }
 
 export class App extends React.Component<AppProps, AppState> {
@@ -24,7 +26,33 @@ export class App extends React.Component<AppProps, AppState> {
 	public constructor(props: AppProps) {
 
 		super(props);
-		this.state = {};
+		this.state = {value: ""};
+		this.handleTextAreaOnChange = this.handleTextAreaOnChange.bind(this);
+		this.handleLoad = this.handleLoad.bind(this);
+		this.handleCompile = this.handleCompile.bind(this);
+
+	}
+
+	private handleTextAreaOnChange(ev: React.ChangeEvent<HTMLTextAreaElement>): void {
+		this.setState({value: ev.target.value});
+	}
+
+	private handleLoad(): void {
+		const path: string[] | undefined = remote.dialog.showOpenDialogSync({properties: ["openFile"], filters: [{
+			name: "*",
+				extensions: ["als"]
+			}]})
+		if (!path) return;
+		const filePath: string = path[0];
+		if (!filePath) return;
+		if (!fs.existsSync(filePath)) return;
+		const data: Buffer | undefined = fs.readFileSync(filePath);
+		if (!data) return;
+		const str = data.toString("utf8");
+		this.setState({value: str});
+	}
+
+	private handleCompile(): void {
 
 	}
 
@@ -33,9 +61,17 @@ export class App extends React.Component<AppProps, AppState> {
 		return (<div className={"App"}>
 			<TopBar/>
 			<div className={"main"}>
-				<WelcomeView/>
+				<textarea onChange={this.handleTextAreaOnChange} className={"editor"} value={this.state.value}/>
+			</div>
+			<div className={"bottomBar"}>
+				<button onClick={this.handleLoad} className={"load"}>Load</button>
+				<button onClick={this.handleCompile} className={"compile"}>Compile</button>
 			</div>
 		</div>);
+
+	}
+
+	public static main(): void {
 
 	}
 }
